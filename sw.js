70c4643c7aa4d669167b947220f338aff90d0fe3
ftcf20-hub <ftcf.20@gmail.com>
2026-07-17 11:32:10 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,1 @@
-self.addEventListener('install', (e) => {
- e.waitUntil(
-   caches.open('ftcf-store').then((cache) => cache.addAll(['/']))
- );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', () => {});
