115e08b8cec94381b1105f91df41b9cb9c9d93ca
ftcf20-hub <ftcf.20@gmail.com>
2026-07-18 11:11:27 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,8 @@
-const CACHE_NAME = 'ftcf-v3'; // Jab bhi koi bada update do, bas ye version number (v2 -> v3) badal dena.
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then((keys) => {
-        return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    }));
-});
-
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
